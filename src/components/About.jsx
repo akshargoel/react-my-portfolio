be6899944fcc817b0fd727abff189fd600e895dc
Akshar goel <goelakshar1212@gmail.com>
2024-07-26 21:10:11 +0530

    project done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
         <div className="max-w-screen-lg px-4 mx-auto flex flex-col justify-center w-full h-full">
-            <div className='pb-8'>
+            <div className="pb-8 mt-24">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
                     About
                 </p>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-44 ">
+    </div>
+  )
+}
+
+export default Footer
